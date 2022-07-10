cebdc5566679f1a1f9174067404cbdbc58dc89ba
Chy Lim <cirshin@gmail.com>
2022-07-10 01:00:17 -0700

    added pokemon description and types

---
+++ README.md
@@ -0,0 +1,2 @@
+A basic Pokedex simulator app created using React
+Made using the PokeApi to fetch Pokemon info
--- src/Entry.js
+++ src/Entry.js
@@ -1,67 +1,84 @@
 import React, { useState, useEffect } from "react";
-import pokemons from "./data";
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
+const numPokemon = 898;
 const url = "https://pokeapi.co/api/v2/pokemon/";
-const Review = () => {
-  const [index, setIndex] = useState(0);
-  // const { name, job, image, text } = pokemons[index];
-  const [pokemonImg, setPokemonImg] = useState(
-    "https://cdn.traction.one/pokedex/pokemon/1.png"
-  );
+const descUrl = "https://pokeapi.co/api/v2/pokemon-species/";
+const imgUrl = "https://cdn.traction.one/pokedex/pokemon/";
 
-  async function fetchPokemon() {
-    const pokemonData = await fetch(url + (index + 1))
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-    return pokemonData;
-  }
+const Entry = () => {
+  const [index, setIndex] = useState(1);
+  const [pokemon, setPokemon] = useState("");
+  const [pokemonDescription, setPokemonDescription] = useState("");
+  const [pokemonTypes, setPokemonTypes] = useState("");
+
+  const getPokemon = async () => {
+    const pokeData = await fetch(descUrl + index);
+    const pokemonData = await pokeData.json();
+
+    setPokemon(pokemonData.name);
+    const cleanedText = pokemonData.flavor_text_entries[1].flavor_text.replace(
+      "\f",
+      " "
+    );
+    setPokemonDescription(cleanedText);
+
+    const pokeInfo = await fetch(url + index);
+    const pokemonInfo = await pokeInfo.json();
+    let types = "";
+    for (let i = 0; i < pokemonInfo.types.length; i++) {
+      types +=
+        pokemonInfo.types[i].type.name.charAt(0).toUpperCase() +
+        pokemonInfo.types[i].type.name.slice(1);
+      if (i != pokemonInfo.types.length - 1) {
+        types += " ";
+      }
+    }
+    console.log(types);
+    setPokemonTypes(types);
+  };
+
+  useEffect(() => {
+    getPokemon();
+  }, [index, getPokemon]);
 
   const checkNumber = (number) => {
-    if (number > pokemons.length - 1) {
-      return 0;
+    if (number > numPokemon) {
+      return 1;
     }
-    if (number < 0) {
-      return pokemons.length - 1;
+    if (number < 1) {
+      return numPokemon;
     }
     return number;
   };
   const nextPokemon = () => {
     let newIndex = checkNumber(index + 1);
-    setPokemonImg(
-      "https://cdn.traction.one/pokedex/pokemon/" + (newIndex + 1) + ".png"
-    );
     setIndex(newIndex);
   };
   const prevPokemon = () => {
     let newIndex = checkNumber(index - 1);
-    setPokemonImg(
-      "https://cdn.traction.one/pokedex/pokemon/" + (newIndex - 1) + ".png"
-    );
     setIndex(newIndex);
   };
   const randomPokemon = () => {
-    let randomNumber = Math.floor(Math.random() * pokemons.length);
+    let randomNumber = Math.floor(Math.random() * numPokemon);
     if (randomNumber === index) {
       randomNumber = index + 1;
     }
     const correctedNum = checkNumber(randomNumber);
     setIndex(checkNumber(correctedNum));
-    setPokemonImg(
-      "https://cdn.traction.one/pokedex/pokemon/" + (correctedNum + 1) + ".png"
-    );
   };
 
   return (
     <article className="review">
       <div className="img-container">
         <img
-          src={pokemonImg}
-          alt={pokemons[index] + "-img"}
+          src={imgUrl + index + ".png"}
+          alt={pokemon + "-img"}
           className="pokemon-img"
         />
       </div>
-      <h4 className="author">{pokemons[index]}</h4>
+      <h3 className="pokemon-name">{pokemon}</h3>
+      <h4 className="type-text">{pokemonTypes}</h4>
       <div className="button-container">
         <button className="prev-btn" onClick={prevPokemon}>
           <FaChevronLeft />
@@ -70,18 +87,12 @@ const Review = () => {
           <FaChevronRight />
         </button>
       </div>
+      <h4>{pokemonDescription}</h4>
       <button className="random-btn" onClick={randomPokemon}>
-        surprise me
-      </button>
-      <button
-        style={{ marginLeft: "1rem" }}
-        className="random-btn"
-        onClick={fetchPokemon}
-      >
-        get pokemon data
+        Random Pokemon
       </button>
     </article>
   );
 };
 
-export default Review;
+export default Entry;
